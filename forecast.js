21147d40bbc109ecb2fee6a08b7c7c3101831d4c
Cendekia Astian <cendiastian@gmail.com>
2023-07-07 17:26:09 +0700

    Soal No 2

---
+++ forecast.js
@@ -0,0 +1,56 @@
+// Import Axios module
+const axios = require('axios');
+
+// API KEY dari akun saya
+const APIKEY = 'ba9259c5b4aef50726ecb5fdab2f4123'
+// function untuk mengambil data ramalan cuaca dari API OpenWeatherMap
+function getWeatherForecast() {
+    // return new Promise(function(resolve, reject) {
+      const url = 'http://api.openweathermap.org/data/2.5/forecast?q=Jakarta,id&appid='+APIKEY;
+      return axios.get(url)
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error('Failed to fetch weather data');
+        }
+        return response.data.list;
+      })
+      .catch(error => {
+        console.log('Error:', error.message);
+        throw error;
+      });
+    }
+  
+  // Fungsi untuk mencetak suhu per hari
+  function printWeatherForecast(forecasts) {
+    const printedDays = new Set();
+
+    forecasts.forEach(forecast => {
+      const date = new Date( forecast.dt * 1000);
+      // let dateFormat = Date.parse("Fri, Jul 7, 2023");
+      const formattedDate = date.toLocaleDateString( 'en-SG',{
+        weekday: 'short',
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric'
+      });
+  
+    // Cek apakah hari sudah dicetak sebelumnya
+    if (!printedDays.has(formattedDate)) {
+
+      // Convert temperature from Kelvin to Celsius
+      const temperature = Math.round(forecast.main.temp - 273.15); 
+
+      console.log(`${formattedDate}: ${temperature}°C`);
+      printedDays.add(formattedDate);
+    }    
+  });
+  }
+  
+  // Mengambil data ramalan cuaca dan mencetaknya
+  getWeatherForecast()
+    .then(forecasts => {
+      printWeatherForecast(forecasts);
+    })
+    .catch(error => {
+      console.log('Error:', error.message);
+    });
